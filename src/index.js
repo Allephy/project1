ec0e1a6980313e44a4e4680dc9ff43dd05bc3381
WinterDrizzle <145249725+WinterDrizzle@users.noreply.github.com>
2023-09-25 17:09:05 +0900

    Found something missed on routing

--- src/index.js
+++ src/index.js
@@ -60,8 +60,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/" element={<Test />}/>
+        <Route path="/*" element={<Home />}/>
       </Routes>
     </div>
   )
