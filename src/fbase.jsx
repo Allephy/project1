f9c4d4d5127dcf8dd2e4bb00a90030e20579195f
Allephy <lightwalk12@gmail.com>
2023-12-18 00:33:36 +0900

    hiding key

--- src/fbase.jsx
+++ src/fbase.jsx
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtE2QKVqDBkM9HK_E57oZ95XdHvLhp6vw",
-  authDomain: "open-source-7f9c3.firebaseapp.com",
-  databaseURL: "https://open-source-7f9c3-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "open-source-7f9c3",
-  storageBucket: "open-source-7f9c3.appspot.com",
-  messagingSenderId: "908903691224",
-  appId: "1:908903691224:web:4a9bcf4f6a22bd84e338ad",
-  measurementId: "G-LRX8HL9V7R",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  
 };
 
 // Initialize Firebase
