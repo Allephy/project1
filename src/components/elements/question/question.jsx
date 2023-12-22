14da25079995317631502a57fd3a072d5c38f123
WinterDrizzle <145249725+WinterDrizzle@users.noreply.github.com>
2023-12-22 17:25:12 +0900

    Update question.jsx

--- src/components/elements/question/question.jsx
+++ src/components/elements/question/question.jsx
@@ -7,7 +7,7 @@ section1의 1번 statement와 section2의 1번 statement 각각의 가중치의 
 */
 
 //Section 1: Drawing: -1 /Pushing: 1
-// 5:5
+// 4:4
 const DPQuestion = [
     {
         id:1,
@@ -17,7 +17,7 @@ const DPQuestion = [
     {
         id:2,
         statement: 1,
-        question:""
+        question:"적극적이라는 말을 많이 듣는다."
     },
     {
         id:3,
@@ -48,23 +48,12 @@ const DPQuestion = [
         id:8,
         statement: -1,
         question:"일부러 연인에게 약간 못되게 굴 때가 있었다"
-    },
-    {
-        id:9,
-        statement: -1,
-        question:"내가 상대를 슬프게 해도 그로 인해 나를 더 사랑하게 된다면 괜찮다"
-    },
-    {
-        id:10,
-        statement: 1,
-        question:"상대가 나를 위해 바뀌기보다는 상대를 위해 나를 바꾼다"
-    },
-
+    }
     
 ]
 
 // Section 2: Change: -1/ Keep: 1
-// Section 2 질문 추가 요청 바람.
+// Section 2 질문 추가 요청 바람. 4:4
 const CKQuestion = [
     {
         id:1,
@@ -88,39 +77,29 @@ const CKQuestion = [
     },
     {
         id:5,
-        statement: -1,
-        question:"상대와 만나는 시간을 일부러 다양하게 한다"
+        statement:-1,
+        question:"상대를 만나기 전과 달라졌다는 말을 많이 듣는다."
     },
     {
         id:6,
-        statement: -1,
-        question:"갑자기 연락을 끊어본 적이 있다"
+        statement: 1,
+        question:"상대가 잘못했다고 해서 이별을 쉽게 생각하지는 않는다"
     },
     {
         id:7,
-        statement: -1,
-        question:"상대가 잘못했을 때 사과하게 만드는 편이다"
-    },
-    {
-        id:8,
-        statement: -1,
-        question:"상대와 약속을 잡을 때 만나는 장소를 일부러 다양하게 한다"
-    },
-    {
-        id:9,
         statement: 1,
         question:"화가 날 때 조곤히 말하는 편이다"
     },
     {
-        id:10,
-        statement: -1,
-        question:"상대를 좋아하는 감정이 갑자기 사라지기도 한다"
+        id:8,
+        statement: 1,
+        question:"상대를 좋아하는 감정이 갑자기 사라지지는 않는다"
     },
     
 ]
 
 //Section 3: Appeal: -1/ Natural: 1
-// 5:5
+// 3:55
 const ANQuestion = [
     {
         id:1,
@@ -135,7 +114,7 @@ const ANQuestion = [
     {
         id:3,
         statement: -1,
-        question:"상대에게 먼저 장난을 친다"
+        question:"내가 가진 매력이 무엇인지 잘 알고 있다."
     },
     {
         id:4,
@@ -144,8 +123,8 @@ const ANQuestion = [
     },
     {
         id:5,
-        statement: 1,
-        question:"나에대해 잘 아는 사람보다 모르는 사람이 나를 좋아한 경우가 많다"
+        statement: -1,
+        question:"나와 친한 사람보다 친하지는 않지만 안면이 있는 사람이 나를 좋아한 경우가 많다"
     },
     {
         id:6,
@@ -160,19 +139,8 @@ const ANQuestion = [
     {
         id:8,
         statement: 1,
-        question:"나를 소개할 때 모호하기보다 정확하게 하려고 노력한다"
-    },
-    {
-        id:9,
-        statement: -1,
-        question:"내가 나를 보는 모습과 사람들이 나를 보는 모습은 꽤나 다르다"
-    },
-    {
-        id:10,
-        statement: 1,
-        question:"나는 파도보다 잔잔한 물결에 가까운 사람이다"
-    },
-    
+        question:"나는 상대에게 어떤 모습을 일부러 보여주려고 한 적은 없지만 상대는 나를 좋아한다"
+    }
 ]
 
 //Section 4: Traditional Gender: 0 / Floating Gender: 1
@@ -188,7 +156,7 @@ const FSQuestion = [
     {
         id:2,
         statement: 1,
-        question:"성격이 매력적인 사람이 외모가 매력적인 사람보다 끌린다"
+        question:"성격을 보는 기준이 외모를 보는 기준보다 까다롭다"
     },
     {
         id:3,
@@ -198,40 +166,30 @@ const FSQuestion = [
     {
         id:4,
         statement: 1,
-        question:"현재 나에게는 신체를 가꾸는 것보다 정신을 고양하는 것이 더 중요하다"
-    },
-    {
-        id:5,
-        statement: 1,
         question:"상대를 알아갈수록 관심이 생기는 편이다"
     },
     {
-        id:6,
+        id:5,
         statement: -1,
-        question:"스킨십을 주도하려고 하며 적극적이다."
+        question:"스킨십을 주도하려고 하며 적극적이다"
     },
     {
-        id:7,
+        id:6,
         statement: 1,
         question:"외모에 대한 칭찬이 성격에 대한 칭찬보다 기억에 더 남는다"
     },
     {
-        id:8,
+        id:7,
         statement: 1,
-        question:"관심이 있는 상대가 있는 자리에 나갈 때 외모를 꾸미기보다 대화거리를 준비하는 편이다"
+        question:"상대가 좋아하는 책이나 영화 등을 추천하면 정말로 본다"
         
     },
     {
-        id:9,
+        id:8,
         statement: -1,
-        question:"헤어진 연인의 신체적 특징이 성격보다 기억에 더 남는다"
-    },
-    {
-        id:10,
-        statement: -1,
-        question:"연인이 자주 바뀌었다"
-    },
-    
+        question:"나는 장거리 연애를 하기는 힘들 것이다."
+        
+    }
 ]
 
 export {
